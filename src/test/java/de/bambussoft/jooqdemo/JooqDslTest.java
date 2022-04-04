3fb0d54794601498ded28cc3dac124b2a61ac90b
Richard Schulz <richard.schulz@soptim.de>
2022-04-04 15:58:31 +0200

    improved column naming

--- src/test/java/de/bambussoft/jooqdemo/JooqDslTest.java
+++ src/test/java/de/bambussoft/jooqdemo/JooqDslTest.java
@@ -3,13 +3,12 @@ package de.bambussoft.jooqdemo;
 import de.bambussoft.jooqdemo.jooq.gen.Tables;
 import de.bambussoft.jooqdemo.jooq.gen.tables.BooleanTests;
 import org.jooq.DSLContext;
-import org.jooq.Result;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jooq.JooqTest;
 
 @JooqTest
-public class JooqDslTest {
+class JooqDslTest {
 
 
     @Autowired
@@ -18,7 +17,7 @@ public class JooqDslTest {
     @Test
     void dslTest() {
         Object stringBoolean = dslContext.selectFrom(Tables.BOOLEAN_TESTS).fetchAny(BooleanTests.BOOLEAN_TESTS.STRING_BOOLEAN);
-        Object intBoolean = dslContext.selectFrom(Tables.BOOLEAN_TESTS).fetchAny(BooleanTests.BOOLEAN_TESTS.INT);
+        Object intBoolean = dslContext.selectFrom(Tables.BOOLEAN_TESTS).fetchAny(BooleanTests.BOOLEAN_TESTS.INT_BOOLEAN);
         assert stringBoolean instanceof Boolean && intBoolean instanceof Boolean;
     }
 }
